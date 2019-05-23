c5ba427a0c90ebe8e624d8c1e9492c514379cb70
Dillon Christensen <kowalski@norfair.local>
2019-05-22 20:13:52 -0500

    updated for norfair

--- stamper.sh
+++ stamper.sh
@@ -2,10 +2,11 @@
 
 FILE=$1
 
-DATE=$(date -r $(echo $FILE | tr -dc '0-9'))
+DATE=$(date -d @$(echo $FILE | tr -dc '0-9'))
 convert -background '#00000080' -font FiraMono-Regular.otf -fill white label:"$DATE" miff:- | composite -gravity southeast -geometry +0+3 - $FILE "$FILE-anno.jpg"
 
 if [ "$?" -eq "0" ]; then
+    echo think we got it
     rm $FILE
     cp "$FILE-anno.jpg" out.jpg
 fi
